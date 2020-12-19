eabfbf11e845e5e06d500f05e5c938a5264e6f38
Dillon Fagan <dillonfagan@pm.me>
2020-12-19 17:52:02 -0500

    Add multiple columns

--- src/routes/board.svelte
+++ src/routes/board.svelte
@@ -1,26 +1,41 @@
 <script>
-    let todos = [
+    let columns = [
         {
-            title: "Empty the trash"
+            title: "To do",
+            tasks: [
+                {
+                    title: "Empty the trash"
+                },
+                {
+                    title: "Cook dinner"
+                },
+                {
+                    title: "Clean the dishes"
+                }
+            ]
         },
         {
-            title: "Cook dinner"
+            title: "Doing",
+            tasks: []
         },
         {
-            title: "Clean the dishes"
+            title: "Done",
+            tasks: []
         }
     ]
 </script>
 
-<div class="h-screen">
-    <div class="w-56 h-full bg-gray-100">
-        <div>To do ({todos.length})</div>
-        <div class="p-4 flex flex-col space-y-3">
-            {#each todos as todo}
-                <div class="bg-white rounded shadow p-2">
-                    <div class="font-bold">{todo.title}</div>
-                </div>
-            {/each}
+<div class="h-screen flex flex-row">
+    {#each columns as column}
+        <div class="w-56 h-full bg-gray-100">
+            <div>{column.title} ({column.tasks.length})</div>
+            <div class="p-4 flex flex-col space-y-3">
+                {#each column.tasks as task}
+                    <div class="bg-white rounded shadow p-2">
+                        <div class="font-bold">{task.title}</div>
+                    </div>
+                {/each}
+            </div>
         </div>
-    </div>
+    {/each}
 </div>
