bb9cc1bfdfe93b56f1fe18821dd5866959ef3ab2
Dillon Fagan <dillonfagan@pm.me>
2020-12-19 17:40:12 -0500

    Introduce todo column

--- src/routes/board.svelte
+++ src/routes/board.svelte
@@ -1,1 +1,14 @@
-<h1>The kanban board will be here.</h1>
+<script>
+    let todos = ["Empty the trash", "Cook dinner", "Clean the dishes"]
+</script>
+
+<div class="h-screen">
+    <div class="w-56 h-full bg-gray-100">
+        <div>To do ({todos.length})</div>
+        <div class="p-4 flex flex-col space-y-3">
+            {#each todos as todo}
+                <div class="bg-white rounded shadow p-2">{todo}</div>
+            {/each}
+        </div>
+    </div>
+</div>
