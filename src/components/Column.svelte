980fc9812f5faae64e287e5fb2e8c375409be065
Dillon Fagan <dillonfagan@pm.me>
2020-12-19 18:29:22 -0500

    Increase column width

--- src/components/Column.svelte
+++ src/components/Column.svelte
@@ -8,7 +8,7 @@
     }
 </script>
 
-<div class="w-56 h-full bg-gray-100">
+<div class="w-64 h-full bg-gray-100">
     <div class="p-4 flex flex-row justify-between font-bold">
         <div>{title} ({tasks.length})</div>
         <button on:click={addTask}>+</button>
