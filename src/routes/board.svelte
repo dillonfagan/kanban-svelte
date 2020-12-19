708f955cb92bc8430efd93f72e5495f31e75085b
Dillon Fagan <dillonfagan@pm.me>
2020-12-19 18:11:02 -0500

    Introduce add buttons

--- src/routes/board.svelte
+++ src/routes/board.svelte
@@ -28,7 +28,10 @@
 <div class="h-screen flex flex-row">
     {#each columns as column}
         <div class="w-56 h-full bg-gray-100">
-            <div class="p-4">{column.title} ({column.tasks.length})</div>
+            <div class="p-4 flex flex-row justify-between font-bold">
+                <div>{column.title} ({column.tasks.length})</div>
+                <button>+</button>
+            </div>
             <div class="px-4 flex flex-col space-y-3">
                 {#each column.tasks as task}
                     <div class="bg-white rounded shadow p-2">
