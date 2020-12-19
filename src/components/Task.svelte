3e6202440cb82d1da60ee498605038c1695a9366
Dillon Fagan <dillonfagan@pm.me>
2020-12-19 18:55:07 -0500

    Makes titles editable

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -1,7 +1,29 @@
 <script>
     export let task;
+
+    let titleInput;
+    let editingTitle = false;
+
+    function editTitle() {
+        console.log(`Editing ${task.title}`);
+        editingTitle = true;
+    }
+
+    function saveTitle() {
+        editingTitle = false;
+
+        const oldTitle = task.title;
+        task.title = titleInput.value;
+        console.log(`${oldTitle} is now ${task.title}`);
+    }
 </script>
 
 <div class="bg-white rounded shadow p-2">
-    <div class="font-bold">{task.title}</div>
+    <input
+        contenteditable={editingTitle}
+        bind:this={titleInput}
+        on:dblclick={editTitle}
+        on:change={saveTitle}
+        value={task.title}
+        class="font-bold" />
 </div>
