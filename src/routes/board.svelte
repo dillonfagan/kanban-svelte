56c8faec2e9e96ef72d9e1e2ec56b5c6bda338cb
Dillon Fagan <dillonfagan@pm.me>
2020-12-19 17:43:21 -0500

    Store tasks in objects

--- src/routes/board.svelte
+++ src/routes/board.svelte
@@ -1,5 +1,15 @@
 <script>
-    let todos = ["Empty the trash", "Cook dinner", "Clean the dishes"]
+    let todos = [
+        {
+            title: "Empty the trash"
+        },
+        {
+            title: "Cook dinner"
+        },
+        {
+            title: "Clean the dishes"
+        }
+    ]
 </script>
 
 <div class="h-screen">
@@ -7,7 +17,9 @@
         <div>To do ({todos.length})</div>
         <div class="p-4 flex flex-col space-y-3">
             {#each todos as todo}
-                <div class="bg-white rounded shadow p-2">{todo}</div>
+                <div class="bg-white rounded shadow p-2">
+                    <div class="font-bold">{todo.title}</div>
+                </div>
             {/each}
         </div>
     </div>
