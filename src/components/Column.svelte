402733d612995ae7431625a92b7bd9ed5f9f423c
Dillon Fagan <dillonfagan@pm.me>
2020-12-19 18:28:36 -0500

    Add tasks

--- src/components/Column.svelte
+++ src/components/Column.svelte
@@ -1,12 +1,17 @@
 <script>
     export let title;
     export let tasks = [];
+
+    function addTask() {
+        let task = { title: "New task" };
+        tasks = [task, ...tasks];
+    }
 </script>
 
 <div class="w-56 h-full bg-gray-100">
     <div class="p-4 flex flex-row justify-between font-bold">
         <div>{title} ({tasks.length})</div>
-        <button>+</button>
+        <button on:click={addTask}>+</button>
     </div>
     <div class="px-4 flex flex-col space-y-3">
         {#each tasks as task}
