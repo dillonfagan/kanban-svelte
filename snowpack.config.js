c8a59e3fbce417d4b119c1d050b3be2a09d48dcd
Dillon Fagan <dillonfagan@pm.me>
2020-12-19 17:20:09 -0500

    Add tailwindcss

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,25 @@
 // Consult https://www.snowpack.dev to learn about these options
 module.exports = {
+	plugins: [
+		[
+			'@snowpack/plugin-build-script',
+			{
+				cmd: "postcss",
+				input: [".css", ".pcss"],
+				output: [".css"],
+			}
+		],
+	
+		[
+			"@snowpack/plugin-svelte",
+			{
+				compilerOptions: {
+					hydratable: true
+				}
+			}
+		],
+	
+	],
 	extends: '@sveltejs/snowpack-config',
 	mount: {
 		'src/components': '/_components'
