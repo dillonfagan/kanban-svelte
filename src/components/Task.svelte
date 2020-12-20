5735771bdceb2c1c243e22f5573d89032a802876
Dillon Fagan <dillonfagan@pm.me>
2020-12-20 15:16:21 -0500

    Remove old task-editing code

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -1,29 +1,7 @@
 <script>
     export let task;
-
-    let titleInput;
-    let editingTitle = false;
-
-    function editTitle() {
-        console.log(`Editing ${task.title}`);
-        editingTitle = true;
-    }
-
-    function saveTitle() {
-        editingTitle = false;
-
-        const oldTitle = task.title;
-        task.title = titleInput.value;
-        console.log(`${oldTitle} is now ${task.title}`);
-    }
 </script>
 
-<div class="bg-white rounded shadow-sm hover:shadow-md p-2">
-    <input
-        contenteditable={editingTitle}
-        bind:this={titleInput}
-        on:dblclick={editTitle}
-        on:change={saveTitle}
-        value={task.title}
-        class="font-bold" />
+<div class="bg-white rounded shadow-sm hover:shadow-md p-2 select-none cursor-move">
+    <div class="font-bold">{task.title}</div>
 </div>
