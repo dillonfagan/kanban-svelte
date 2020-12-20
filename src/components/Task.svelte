c767c80b1cba35cfedbbf506493cb0092540500a
Dillon Fagan <dillonfagan@pm.me>
2020-12-20 15:24:22 -0500

    Select task

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -1,7 +1,18 @@
 <script>
     export let task;
+
+    let selected = false;
 </script>
 
-<div class="bg-white rounded shadow-sm hover:shadow-md p-2 select-none cursor-move">
+<div 
+    on:click={() => selected = !selected}
+    class:selected
+    class="bg-white rounded shadow-sm hover:shadow-md p-2 select-none cursor-move">
     <div class="font-bold">{task.title}</div>
 </div>
+
+<style>
+    .selected {
+        @apply bg-yellow-300;
+    }
+</style>
