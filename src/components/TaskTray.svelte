982c723d86e65b0e1f1eb07360b33eb3e7ff9344
Dillon Fagan <dillonfagan@pm.me>
2020-12-20 16:04:12 -0500

    Add title input to TaskTray

--- src/components/TaskTray.svelte
+++ src/components/TaskTray.svelte
@@ -2,6 +2,13 @@
     export let task;
 </script>
 
-<aside class="h-screen w-96 flex flex-col fixed top-0 right-0 bg-gray-400">
-    <div>{task.title}</div>
+<aside class="h-screen w-96 p-4 flex flex-col fixed top-0 right-0 bg-gray-400">
+    <div class="font-bold text-xl">{task.title}</div>
+    <div class="py-4 flex flex-col space-y-1">
+        <label for="title" class="text-sm">Title</label>
+        <input
+            id="title"
+            bind:value={task.title} 
+            class="p-2 rounded" />
+    </div>
 </aside>
