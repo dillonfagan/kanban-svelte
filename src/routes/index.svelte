2f175fc249479f4ba41c9a36d43dca6c1ebd5487
Dillon Fagan <dillonfagan@pm.me>
2020-12-20 14:59:05 -0500

    Move board to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,26 @@
-<h1>This is where your projects will be listed.</h1>
+<script>
+    import { onMount } from "svelte";
+    import Projects from "./_data/projects";
+    import Column from "$components/Column.svelte";
+
+    let project;
+    let columns = [];
+
+    onMount(() => {
+        const projects = Projects.load();
+        project = projects[0];
+        columns = project.columns;
+    });
+
+    function addTask(event) {
+        const { column, task } = event.detail;
+        columns[column].tasks = [task, ...columns[column].tasks];
+        console.log(columns);
+    }
+</script>
+
+<div class="h-screen flex flex-row">
+    {#each columns as column, i}
+        <Column id={i} title={column.title} tasks={column.tasks} on:taskAdded={addTask} />
+    {/each}
+</div>
