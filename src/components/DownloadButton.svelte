97deb41aa7357394372ee7f7b5a65b2bb862b219
Dillon Fagan <dillonfagan@pm.me>
2020-12-21 18:48:52 -0500

    Pull out DownloadButton

---
+++ src/components/DownloadButton.svelte
@@ -0,0 +1,9 @@
+<button on:click class="h-12 w-12 rounded-full shadow-lg fixed bottom-8 right-8 z-10 bg-black text-white hover:text-green-200 flex justify-center items-center">
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-3/5 h-3/5" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+        <line x1="12" y1="11" x2="12" y2="17" />
+        <polyline points="9 14 12 17 15 14" />
+    </svg>
+</button>
