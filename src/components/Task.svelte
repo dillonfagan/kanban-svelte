a3faa4361402d96f4116c73d524553ac108f3ca2
Dillon Fagan <dillonfagan@pm.me>
2020-12-24 10:39:48 -0500

    Introduce drag and drop to Task

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -12,10 +12,21 @@
             task: task
         });
     }
+
+    function drag() {
+        console.log('Dragging...');
+    }
+
+    function drop() {
+        console.log('Dropped.');
+    }
 </script>
 
 <div
+    draggable="true"
     on:click={select}
+    on:dragstart={drag}
+    on:dragend={drop}
     class="bg-white rounded box-border border-2 border-gray-200 hover:border-black text-gray-600 hover:text-black hover:shadow p-2 select-none cursor-move">
     <div class="font-bold">{task.title}</div>
 </div>
