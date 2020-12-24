1b6648b1356b2a7b013fbded2dfaf3d545baca55
Dillon Fagan <dillonfagan@pm.me>
2020-12-24 15:16:17 -0500

    Simplify load and dump logs

--- src/data/projects.js
+++ src/data/projects.js
@@ -22,13 +22,13 @@ function load() {
     // localStorage.clear();
     const projectsData = JSON.parse(localStorage.getItem('projects'));
     const projects = projectsData || [defaultProject];
-    console.log(`Loading Projects:\n${JSON.stringify(projects)}`);
+    console.log(`Loaded ${projects.length} projects.`);
     return projects;
 }
 
 function dump(projectsData) {
     const stringified = JSON.stringify(projectsData);
-    console.log(`Dumping:\n${stringified}`);
+    console.log(`Dumped ${projectsData.length} projects.`);
     localStorage.setItem('projects', stringified);
 }
 
